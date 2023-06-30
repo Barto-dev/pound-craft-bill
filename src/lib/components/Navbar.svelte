1ed52f9c8b0c240a1a0fb112d9d272e89a4f4f03
Illia <illya1004work@gmail.com>
2023-06-30 09:09:05 +0100

    feat: added Navbar component

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,40 @@
+<script>
+  import { page } from '$app/stores';
+</script>
+
+<header class="col-span-3 bg-daisyBush text-center">
+  <div>
+    <a href="/invoices">
+      <img src="/images/logo.svg" alt="Pound Bill Craft" class="mx-auto" />
+    </a>
+  </div>
+  <nav aria-label="Main navigation" class="nav">
+    <ul class="list-none text-2xl font-bold">
+<!--      TODO add aria current-->
+      <li><a class:active={$page.url.pathname === '/invoices'} href="/invoices">Invoices</a></li>
+      <li><a class:active={$page.url.pathname === '/clients'} href="/clients">Clients</a></li>
+      <li><a href="/settings">Settings</a></li>
+      <li><a href="/logout">Logout</a></li>
+    </ul>
+  </nav>
+</header>
+
+<style lang="postcss">
+  .nav li {
+    @apply mb-6;
+  }
+
+  .nav a {
+    @apply font-bold text-white hover:text-goldenFizz;
+  }
+
+  .nav a.active {
+    @apply px-8 text-robinEggBlue transition-[padding];
+    background: url('/images/active-nav-left.svg') left top no-repeat,
+      url('/images/active-nav-right.svg') right top no-repeat;
+  }
+
+  .nav a.active:hover {
+    @apply px-9;
+  }
+</style>
--- src/routes/(dashboard)/+layout.svelte
+++ src/routes/(dashboard)/+layout.svelte
@@ -1,38 +1,10 @@
+<script lang="ts">
+  import Navbar from '$lib/components/Navbar.svelte';
+</script>
+
 <div class="grid min-h-screen grid-cols-12 gap-x-12 bg-whisper">
-  <header class="col-span-3 bg-daisyBush text-center">
-    <div>
-      <a href="/invoices">
-        <img src="/images/logo.svg" alt="Pound Bill Craft" class="mx-auto" />
-      </a>
-    </div>
-    <nav aria-label="Main navigation" class="nav">
-      <ul class="list-none text-2xl font-bold">
-        <li class="">
-          <a href="/invoices" aria-current="page">Invoices</a>
-        </li>
-        <li class="">
-          <a href="/clients">Clients</a>
-        </li>
-        <li class="">
-          <a href="/settings">Settings</a>
-        </li>
-        <li class="">
-          <a href="/logout">Logout</a>
-        </li>
-      </ul>
-    </nav>
-  </header>
+  <Navbar />
   <main class="col-span-8 pt-10">
     <slot />
   </main>
 </div>
-
-<style lang="postcss">
-  .nav li {
-    @apply mb-6;
-  }
-
-  .nav a {
-    @apply font-bold text-white hover:text-goldenFizz;
-  }
-</style>
