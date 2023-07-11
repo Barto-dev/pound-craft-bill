b91c9affd82080090f2af4bf24bad4f44c89df00
Illia <illya1004work@gmail.com>
2023-07-11 08:44:52 +0100

    feat: added icons support for Button component

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
   import cn from 'classnames';
+  import { SvelteComponent } from 'svelte';
 
   export let onClick: () => void;
-  export let variant: 'primary' | 'secondary' | 'danger' = 'primary';
+  export let variant: 'primary' | 'secondary' | 'danger' | 'outline' | 'text' = 'primary';
   export let isAnimated = false;
+  export let iconLeft: (new (...args: any[]) => SvelteComponent) | null = null;
+  export let iconRight: (new (...args: any[]) => SvelteComponent) | null = null;
 </script>
 
 <button
@@ -12,14 +15,24 @@
   class:primary={variant === 'primary'}
   class:secondary={variant === 'secondary'}
   class:danger={variant === 'danger'}
+  class:outline={variant === 'outline'}
+  class:text={variant === 'text'}
   on:click|preventDefault={onClick}
 >
+  {#if iconLeft}
+    <svelte:component this={iconLeft} class="mr-2" />
+  {/if}
   <slot />
+  {#if iconRight}
+    <svelte:component this={iconRight} class="ml-2" />
+  {/if}
 </button>
 
 <style lang="postcss">
   .btn {
     @apply relative
+    flex
+    items-center
     whitespace-nowrap
     rounded-lg
     px-5
@@ -48,5 +61,13 @@
 
   .danger {
     @apply bg-scarlet text-goldenFizz;
+  }
+
+  .outline {
+    @apply border-daisyBush text-daisyBush hover:bg-daisyBush hover:text-white;
+  }
+
+  .text {
+    @apply bg-transparent px-0 text-scarlet underline hover:no-underline;
   }
 </style>
