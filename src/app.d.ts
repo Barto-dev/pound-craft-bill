1018068a57e998d5be1afc7a82bbd232a26cd6a5
Illia <60040628+Barto-dev@users.noreply.github.com>
2023-07-31 18:00:39 +0100

    feat: add superbase server hook

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,12 +1,16 @@
-// See https://kit.svelte.dev/docs/types#app
-// for information about these interfaces
+import type { SupabaseClient, Session } from '@supabase/supabase-js'
+import type { Database } from './types/supabase';
+
 declare global {
   namespace App {
+    interface Locals {
+      supabase: SupabaseClient<Database>
+      getSession(): Promise<Session | null>
+    }
+    interface PageData {
+      session: Session | null
+    }
     // interface Error {}
-    // interface Locals {}
-    // interface PageData {}
     // interface Platform {}
   }
 }
-
-export {};
